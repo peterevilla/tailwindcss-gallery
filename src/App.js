31bb23f193a1172a0df5f46ec802d0e367cd1174
Pedro Revilla <peterevilla@gmail.com>
2020-04-16 16:47:50 -0500

    Image Gallery using tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ImageCard from "./components/ImageCard";
+import Search from "./components/Search";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    axios
+      .get(
+        `https://pixabay.com/api/?key=16078376-69de63e0e56115c681d66e383&q=${query}&image_type=photo`
+      )
+      .then((response) => {
+        console.log(response.data);
+
+        setIsLoading(false);
+
+        setImages(response.data.hits);
+      })
+      .catch((err) => console.log(err));
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <Search handleSubmit={(text) => setQuery(text)} />
+      {!isLoading && images.length === 0 && <h1 className='text-3xl text-center'>No images found</h1>}
+      {isLoading ? (
+        <h1 className="text-3xl text-center">Loading...</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((item) => (
+            <ImageCard key={item.id} images={item} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/ImageCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const ImageCard = (props) => {
+  const tags = props.images.tags.split(",");
+  return (
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <img class="w-full" src={`${props.images.webformatURL}`} />
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">Photo by {props.images.user}</div>
+        <ul class="text-gray-700 text-base">
+          <li>
+            <strong>Views: {props.images.views}</strong>
+          </li>
+          <li>
+            <strong>Downloads: {props.images.downloads}</strong>
+          </li>
+          <li>
+            <strong>Likes: {props.images.likes}</strong>
+          </li>
+        </ul>
+      </div>
+      <div class="px-6 py-4">
+        {tags.map((item) => (
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            {item}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageCard;
